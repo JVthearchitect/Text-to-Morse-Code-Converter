da9978af7a3d2c7bfe5199886b01bed4486c8c0d
JVthearchitect <120519530+JVthearchitect@users.noreply.github.com>
2023-12-13 18:52:32 -0500

    remove unnecessary comments in code to clean it up

--- main.py
+++ main.py
@@ -7,19 +7,7 @@ def ConvertingMorse(given_input):
         morse_output += morse_code_dict[char] + " "
     return morse_output
 
-# library or dictionary of morse code symbols and the alphabet
-
-# convert each letter and space to morse code
-
-# replace() letters with the morse code equivalent
-
-#have exceptions to push past the use of symbols that aren't in the morse code
-
-#change all letter inputs into lower case
 
 given_input = input("What would you like converted? ")
 morse_output = ConvertingMorse(given_input)
 print(morse_output)
-
-
-#convert input into morse code - maybe use a Class to make this happen (or is it called a function?)
